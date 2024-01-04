7594531ae57d7e9385e5b975abab01b34ca2733d
yahyagulshan <m.yahya.gulshan@gmail.com>
2024-01-04 22:41:59 +0500

    new changes on code pipeline branch

--- terraform-run-with-aws-vpc/data_sources.tf
+++
@@ -1,9 +0,0 @@
-##########################################
-##         Data Source for AWS          ##
-##########################################
-
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
-
-data "aws_availability_zones" "available" {}
